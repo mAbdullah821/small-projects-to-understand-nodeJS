e351bec5277a3888c3ca02afa81bb981ecbd3148
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-06-19 16:36:57 +0300

    Test the directory reorder project again

--- 00- [Project] Directories Reorderer/config.js
+++ 00- [Project] Directories Reorderer/config.js
@@ -1,7 +1,7 @@
 const rootPath = '..';
 const startFromOrder = 1;
-const increaseBy = 0; // can be [positive (or) 0 (or) negative]
-const paddingLength = 5;
+const increaseBy = 10; // can be [positive (or) 0 (or) negative]
+const paddingLength = 3;
 const orderedDirectoryNameRegex = /[0-9.]+-( |%20)/;
 
 module.exports = {
