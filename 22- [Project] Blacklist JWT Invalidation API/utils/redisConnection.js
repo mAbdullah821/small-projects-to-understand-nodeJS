e25f4b4dc3eacf4d24a79d0bdd460e9fda1ab56f
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-06-20 18:44:10 +0300

    Add redis' ready event to notify the app when redis is connected

--- 22- [Project] Blacklist JWT Invalidation API/utils/redisConnection.js
+++ 22- [Project] Blacklist JWT Invalidation API/utils/redisConnection.js
@@ -4,6 +4,8 @@ const redisClient = redis.createClient(); // REDIS_PORT = 6379;
 
 redisClient.on('error', (err) => console.log('Redis Client Error:-> ', err));
 
+redisClient.on('ready', () => console.log('Redis Connected!'));
+
 redisClient.connect();
 
 module.exports = redisClient;
