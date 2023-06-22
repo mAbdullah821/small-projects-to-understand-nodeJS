6dd0259a1ccadb7b225693663705395eabfd222b
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-06-22 16:25:30 +0300

    Update the order of the middleware after adding the new ones

--- 22- [Project] Blacklist JWT Invalidation API/app.js
+++ 22- [Project] Blacklist JWT Invalidation API/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { SERVER_PORT } = require('./utils/config');
-const { isAdmin, validateToken, isTokenValid } = require('./middleware');
 const { sayHiUser } = require('./controllers/user-controllers');
 const authRoutes = require('./routes/auth-routes');
 const userRoutes = require('./routes/user-routes');
 const adminRoutes = require('./routes/admin-routes');
+const {
+  extractTokenData,
+  isWhitelistedToken,
+  validateToken,
+  isTokenValid,
+  isAdmin,
+} = require('./middleware');
 
 const app = express();
 
 app.use(express.json());
 app.use('/', authRoutes);
+
+app.use(extractTokenData);
+app.use(isWhitelistedToken);
 app.use(validateToken);
 
 app.get('/', sayHiUser);
