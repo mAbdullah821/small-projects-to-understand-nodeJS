1283251e1c2bbbe1190d922735b11a4deeaf0261
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-06-22 15:50:32 +0300

    Update token service
    	- Replace the magic number in getExpiryTime().
    	- Modify the JWT payload structure to be more generalized in
    	  issueToken().
    	- Remove the token payload extraction logic from validateToken()
    	  to make it have only one single responsibility, and change the
    	  function return.

--- 22- [Project] Blacklist JWT Invalidation API/services/token-service.js
+++ 22- [Project] Blacklist JWT Invalidation API/services/token-service.js
@@ -3,51 +3,46 @@ const { JWT_SECRET, JWT_TTL } = require('../utils/config');
 const jwtInvalidationService = require('./jwt-invalidation-service');
 
 const getExpiryTime = (TTLInSeconds) => {
-  return Math.floor(Date.now() / 1000) + TTLInSeconds;
+  const millisecondsInOneSecond = 1000;
+  return Math.floor(Date.now() / millisecondsInOneSecond) + TTLInSeconds;
 };
 
 const issueToken = async (payload) => {
   const expireAt = getExpiryTime(JWT_TTL);
-  const token = await jwt.sign({ exp: expireAt, data: payload }, JWT_SECRET);
+  const token = await jwt.sign({ exp: expireAt, ...payload }, JWT_SECRET);
   return { token, expireAt };
 };
 
-const validateToken = async (token) => {
-  let valid = false;
-  let data = null;
+const validateToken = async (tokenData) => {
+  const { userId, deviceId, rule, iat: issuedAt } = tokenData;
+
+  const blacklistedAtArr = [];
+
+  blacklistedAtArr.push(
+    jwtInvalidationService.getUserDeviceBlacklistedAt(userId, deviceId)
+  );
+  blacklistedAtArr.push(
+    jwtInvalidationService.getUserAllDevicesBlacklistedAt(userId)
+  );
+  blacklistedAtArr.push(
+    jwtInvalidationService.getAllUsersDevicesBlacklistedAt()
+  );
+
+  let result;
 
   try {
-    const payload = await jwt.verify(token, JWT_SECRET);
-
-    const { userId, deviceId } = payload.data;
-    const issuedAt = payload.iat;
-
-    const blacklistedAtArr = [];
-
-    blacklistedAtArr.push(
-      jwtInvalidationService.getUserDeviceBlacklistedAt(userId, deviceId)
-    );
-    blacklistedAtArr.push(
-      jwtInvalidationService.getUserAllDevicesBlacklistedAt(userId)
-    );
-    blacklistedAtArr.push(
-      jwtInvalidationService.getAllUsersDevicesBlacklistedAt()
-    );
-
-    const result = await Promise.all(blacklistedAtArr);
-
-    const blacklistedAt = Math.max(...result);
-
-    if (blacklistedAt && issuedAt <= blacklistedAt)
-      throw new Error('Blacklisted token');
-
-    valid = true;
-    data = payload.data;
+    result = await Promise.all(blacklistedAtArr);
   } catch (err) {
     console.log(err);
-  } finally {
-    return { valid, data };
+    throw new Error('Inter Database server error!');
   }
+
+  const blacklistedAt = Math.max(...result);
+
+  if (blacklistedAt && issuedAt <= blacklistedAt)
+    throw new Error('Blacklisted token');
+
+  return { userId, deviceId, rule };
 };
 
 module.exports = {
