0dcf8f51b0ea238eafbc9f186d396b3cda77da8c
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-06-22 16:10:14 +0300

    Uses the caching functions && try{} catch(){}
    		For:
    		 - Caching invalidation.
    		 - Errors catching and handling.

--- 22- [Project] Blacklist JWT Invalidation API/controllers/admin-controllers.js
+++ 22- [Project] Blacklist JWT Invalidation API/controllers/admin-controllers.js
@@ -1,31 +1,40 @@
 const deviceModel = require('../models/device-model');
 const jwtInvalidationService = require('../services/jwt-invalidation-service');
+const whitelistService = require('../services/whitelist-cache-service');
 
 const invalidateUsers = async (req, res, next) => {
   const type = req.params.type;
   const userId = req.params.userId;
   let done = null;
 
-  if (type === 'singleUser') {
-    const tokenExpiration = await deviceModel.getMaxTokenExpirationFromUserId(
-      userId
-    );
-    done = await jwtInvalidationService.invalidateUserAllDevices(
-      userId,
-      tokenExpiration
-    );
-  } else if (type === 'allUsers') {
-    const tokenExpiration = await deviceModel.getMaxTokenExpiration();
-    done = await jwtInvalidationService.invalidateAllUsersDevices(
-      tokenExpiration
-    );
-  } else {
-    return next(new Error('Not valid path!'));
+  try {
+    if (type === 'singleUser') {
+      const tokenExpiration = await deviceModel.getMaxTokenExpirationFromUserId(
+        userId
+      );
+      done = await jwtInvalidationService.invalidateUserAllDevices(
+        userId,
+        tokenExpiration
+      );
+
+      await whitelistService.removeAllUserTokensFromWhitelist(userId);
+    } else if (type === 'allUsers') {
+      const tokenExpiration = await deviceModel.getMaxTokenExpiration();
+      done = await jwtInvalidationService.invalidateAllUsersDevices(
+        tokenExpiration
+      );
+
+      await whitelistService.removeAllUsersTokensFromWhitelist();
+    } else {
+      throw new Error('Not valid path!');
+    }
+
+    if (!done) throw new Error('Unable to invalidate tokens!');
+
+    res.send({ msg: 'Invalidate tokens successfully :)' });
+  } catch (err) {
+    next(err);
   }
-
-  if (!done) return next(new Error('Unable to invalidate tokens!'));
-
-  res.send({ msg: 'Invalidate tokens successfully :)' });
 };
 
 module.exports = {
