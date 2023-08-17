dc216987be0539228905e9193d7bf1655bc8d5ab
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-17 16:37:46 +0300

    Fix orderTotal is not-defined

--- Unit Testing/app.js
+++ Unit Testing/app.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { orderTotal_1, orderTotal_2 } = require('./order-total.js');
 
-const cart = {
+const cart1 = {
   orders: [
     { price: 30, quantity: 3 },
     { price: 15, quantity: 2 },
@@ -20,5 +20,16 @@ const cart2 = {
   ],
 };
 
-orderTotal(axios, cart).then((cost) => console.log('Orders-Cost: ', cost));
-orderTotal(axios, cart2).then((cost) => console.log('Orders-Cost: ', cost));
+orderTotal_1(axios, cart1).then((cost) =>
+  console.log('orderTotal_1 - cart1 - Orders-Cost: ', cost)
+);
+orderTotal_1(axios, cart2).then((cost) =>
+  console.log('orderTotal_1 - cart2 - Orders-Cost: ', cost)
+);
+
+orderTotal_2(cart1).then((cost) =>
+  console.log('orderTotal_2 - cart1 - Orders-Cost: ', cost)
+);
+orderTotal_2(cart2).then((cost) =>
+  console.log('orderTotal_2 - cart2 - Orders-Cost: ', cost)
+);
