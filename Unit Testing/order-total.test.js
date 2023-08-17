35434ea894ab231ec323e64fb28918912a84cef4
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-17 15:05:06 +0300

    Remove redundant code and make better test messages

--- Unit Testing/order-total.test.js
+++ Unit Testing/order-total.test.js
@@ -3,12 +3,12 @@ const { orderTotal_1, orderTotal_2 } = require('./order-total.js');
 
 jest.mock('axios');
 
-test('Correct orders cost', () =>
+test('orderTotal_1 - Correct orders cost', () =>
   orderTotal_1(null, {
     orders: [{ price: 30 }, { price: 15 }, { price: 5 }],
   }).then((value) => expect(value).toBe(50)));
 
-test('add quantity to the order', () =>
+test('orderTotal_1 - add quantity to the order', () =>
   orderTotal_1(null, {
     orders: [
       { price: 30, quantity: 3 },
@@ -17,7 +17,7 @@ test('add quantity to the order', () =>
     ],
   }).then((value) => expect(value).toBe(125)));
 
-test('add shipping cost to the order', () =>
+test('orderTotal_1 - add shipping cost to the order', () =>
   orderTotal_1(null, {
     orders: [
       { price: 30, quantity: 3 },
@@ -27,7 +27,7 @@ test('add shipping cost to the order', () =>
     ],
   }).then((value) => expect(value).toBe(160)));
 
-test('fetch country vat then calculate Orders-Cost with custom mock function', () => {
+test('orderTotal_1 - fetch country vat then calculate Orders-Cost with custom mock function', () => {
   const fakeAxios = {
     get: (url) => {
       expect(url).toBe('https://jsonplaceholder.typicode.com/todos/20');
@@ -49,7 +49,7 @@ test('fetch country vat then calculate Orders-Cost with custom mock function', (
   }).then((value) => expect(value).toBe(192));
 });
 
-test('fetch country vat then calculate Orders-Cost with module mocking', () => {
+test('orderTotal_2 - fetch country vat then calculate Orders-Cost with module mocking', () => {
   axios.get.mockResolvedValue({ data: { id: 20 } });
 
   return orderTotal_2({
@@ -62,31 +62,3 @@ test('fetch country vat then calculate Orders-Cost with module mocking', () => {
     ],
   }).then((value) => expect(value).toBe(192));
 });
-
-// const myMock2 = jest.fn();
-// const a = { a: 123 };
-// const bound1 = myMock2.bind(a);
-// bound1();
-// const b = { b: 123 };
-// const bound = myMock2.bind(b);
-// bound();
-console.log('-------------------------');
-// console.log(myMock2.mock.contexts);
-
-// const myMock1 = jest.fn();
-// myMock1.mockReturnValueOnce(10).mockReturnValueOnce('x').mockReturnValue(true);
-// // const c = new myMock1({ name: 'memo' });
-// // console.log(myMock1.mock.instances[0]);
-// console.log(myMock1(), myMock1());
-
-const filterTestFn = jest.fn();
-
-// Make the mock return `true` for the first call,
-// and `false` for the second call
-filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
-
-const result = [11, 12].filter((num) => filterTestFn(num * 2));
-
-console.log(result);
-console.log(filterTestFn.mock.calls[0][0]); // 11
-console.log(filterTestFn.mock.calls[1][0]); // 12
