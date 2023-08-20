b4673e6b2304ac9c1a5e5fdec2672e76baaf96ec
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-20 21:02:35 +0300

    Use variant maps operators

--- RxJS/operators.js
+++ RxJS/operators.js
@@ -6,6 +6,23 @@ const {
   retry,
   throwError,
   concat,
+  delay,
+  mergeMap,
+  concatMap,
+  switchMap,
+  exhaustMap,
+  forkJoin,
+  interval,
+  take,
+  tap,
+  Observable,
+  takeWhile,
+  timer,
+  combineLatest,
+  endWith,
+  sequenceEqual,
+  skipWhile,
+  iif,
 } = require('rxjs');
 
 const stringList = ['memo', 'amo', 'me', 'do'];
@@ -49,3 +66,41 @@ concat(
     (value) => console.log(value)
     // (err) => console.log(err.message)
   );
+
+// ----->  Map, switchMap, mergeMap, flatMap, concatMap, exhaustMap
+
+const example = (operator) => () => {
+  from([0, 1, 2, 3, 4, 5])
+    .pipe(operator((value) => of(value).pipe(delay(500))))
+    .subscribe({
+      next: (value) => console.log(value),
+      error: () => {},
+      complete: () => console.log(operator.name + ' Completed!'),
+    });
+};
+
+console.log('-----------------');
+// example(mergeMap)(); // like (Promise.all)
+// example(concatMap)(); // wait the previous observable before continue
+// example(switchMap)(); // cancel previous observable
+// example(exhaustMap)(); //
+
+const observer = interval(1000)
+  .pipe(
+    takeWhile((val) => val < 2),
+    switchMap(() => interval(100)),
+    mergeMap((val) =>
+      iif(
+        () => val <= 10,
+        of(val),
+        tap(() => observer.unsubscribe())
+      )
+    )
+
+    // skipWhile((val) => val <= 10),
+    // ,
+    // endWith('Friend')
+  )
+  .subscribe(console.log);
+
+//TODO: Solve the error in the previous code
